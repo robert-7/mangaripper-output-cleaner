e6e2ffcb81bfee2fa5f9ae7e78b51b2c063b4b88
Robert Lech <robert.lech123@gmail.com>
2017-06-28 22:20:30 -0400

    Bug fixes. Configuration file.
    This was tested with many other mangas and a lot more exceptions are caught and dealt with accordingly.
    
    Also, we now handle a configuration file with a lot of checks in place to ensure bad input is caught early on.

--- convert.py
+++ convert.py
@@ -3,15 +3,88 @@ from glob import glob
 import numpy as np
 import shutil
 import re
+from configparser import ConfigParser
 
-CHAPTER_FOLDER_TEMPLATE = "Shingeki no Kyojin Gaiden - Kuinaki Sentaku {}"
-CHAPTER_RANGE_START  = 0.5
-CHAPTER_RANGE_FINISH = 9
-CHAPTER_INCREMENT = 0.5
-BACKUP_BOOLEAN = True
-BACKUP_DIRECTORY = "backup"
-ZFILL_LENGTH = 5
+CONFIG_FILE = "config.ini"
+MANGA_SECTION = "manga"
+TESTING_SECTION = "testing"
 
+config = ConfigParser()
+config.read(CONFIG_FILE)
+
+# parse TITLE
+TITLE = config[MANGA_SECTION]["TITLE"]
+CHAPTER_FOLDER_TEMPLATE = TITLE + " {}"
+# TODO: Add Check here
+
+# parse CHAPTER_RANGE_START
+CHAPTER_RANGE_START = config[MANGA_SECTION]["CHAPTER_RANGE_START"]
+try:
+    CHAPTER_RANGE_START = int(CHAPTER_RANGE_START)
+    if CHAPTER_RANGE_START <= 0:
+        raise Exception()
+except:
+    error_msg = "CHAPTER_RANGE_START must be whole number greater than zero. Currently, it is: {}"
+    raise Exception(error_msg.format(CHAPTER_RANGE_START))
+    
+# parse CHAPTER_RANGE_FINISH
+CHAPTER_RANGE_FINISH = config[MANGA_SECTION]["CHAPTER_RANGE_FINISH"]
+try:
+    CHAPTER_RANGE_FINISH = int(CHAPTER_RANGE_FINISH)
+    if CHAPTER_RANGE_FINISH <= CHAPTER_RANGE_START:
+        raise Exception()
+except:
+    error_msg = "CHAPTER_RANGE_FINISH must be whole number greater than zero. Currently, it is: {}"
+    raise Exception(error_msg.format(CHAPTER_RANGE_FINISH))
+
+# parse CHAPTER_INCREMENT
+CHAPTER_INCREMENT = config[MANGA_SECTION]["CHAPTER_INCREMENT"]
+if CHAPTER_INCREMENT == "0.5":
+    CHAPTER_INCREMENT = 0.5
+elif CHAPTER_INCREMENT == "1":
+    CHAPTER_INCREMENT = 1
+else:
+    error_msg = "CHAPTER_INCREMENT must be 0.5 or 1. Currently, it is: {}"
+    raise Exception(error_msg.format(CHAPTER_INCREMENT))
+
+# parse BACKUP_BOOLEAN
+BACKUP_BOOLEAN = config[MANGA_SECTION]["BACKUP_BOOLEAN"]
+try:
+    BACKUP_BOOLEAN = bool(BACKUP_BOOLEAN)
+except:
+    error_msg = "BACKUP_BOOLEAN must be either True or False. Currently, it is: {}"
+    raise Exception(error_msg.format(BACKUP_BOOLEAN))
+
+# obtain BACKUP_DIRECTORY
+BACKUP_DIRECTORY = config[MANGA_SECTION]["BACKUP_DIRECTORY"]
+
+# parse ZFILL_LENGTH
+ZFILL_LENGTH = config[MANGA_SECTION]["ZFILL_LENGTH"]
+try:
+    ZFILL_LENGTH = int(ZFILL_LENGTH)
+except:
+    error_msg = "ZFILL_LENGTH must be whole number greater than zero. Currently, it is: {}"
+    raise Exception(error_msg.format(ZFILL_LENGTH))
+    
+# parse DEBUG_BOOLEAN
+DEBUG_BOOLEAN = config[TESTING_SECTION]["DEBUG_BOOLEAN"]
+try:
+    DEBUG_BOOLEAN = bool(DEBUG_BOOLEAN)
+except:
+    error_msg = "DEBUG_BOOLEAN must be either True or False. Currently, it is: {}"
+    raise Exception(error_msg.format(DEBUG_BOOLEAN))
+
+# print all config values if we're in debug mode    
+if DEBUG_BOOLEAN:
+    print("Printing values since DEBUG_BOOLEAN is True")
+    print("CHAPTER_FOLDER_TEMPLATE = {}".format(CHAPTER_FOLDER_TEMPLATE))
+    print("CHAPTER_RANGE_START = {}".format(CHAPTER_RANGE_START))
+    print("CHAPTER_RANGE_FINISH = {}".format(CHAPTER_RANGE_FINISH))
+    print("CHAPTER_INCREMENT = {}".format(CHAPTER_INCREMENT))
+    print("BACKUP_BOOLEAN = {}".format(BACKUP_BOOLEAN))
+    print("BACKUP_DIRECTORY = {}".format(BACKUP_DIRECTORY))
+    print("ZFILL_LENGTH = {}".format(ZFILL_LENGTH))
+    
 def get_number_of_images(path=".", extension=".jpg"):
   list_dir = []
   list_dir = os.listdir(path)
@@ -49,21 +122,27 @@ def backup():
                 os.chdir("..")
                 copytree(chapter_folder, BACKUP_DIRECTORY + "/" + chapter_folder)
                 chapters.append(chapter)
+    
     return chapters
             
 def convert():
     chapters = backup()
     
     for chapter in chapters:
+        if DEBUG_BOOLEAN:
+            print("Chapter {}".format(chapter))
         chapter_folder = CHAPTER_FOLDER_TEMPLATE.format(chapter)
         os.chdir(chapter_folder)
-        
         number_of_images = get_number_of_images()
-        glob_caught_images = glob('*[0-9][0-9][0-9]*.jpg')
+        glob_regex = '*[0-9][0-9][0-9]*.jpg'
+        glob_caught_images = glob(glob_regex)
         number_of_images_by_glob = len(glob_caught_images)
         if number_of_images != number_of_images_by_glob:
-            error_message = "Glob missed some images: total={}, glob_caught={}"
-            raise Exception(error_message.format(number_of_images, number_of_images_by_glob))
+            glob_missed_images = set(glob("*")) - set(glob(glob_regex))
+            for missed in glob_missed_images:
+                if ("cred" not in missed):
+                    error_message = "Glob missed some images: total={}, glob_caught={}"
+                    raise Exception(error_message.format(number_of_images, number_of_images_by_glob))
         
         duplicate_handler = 1
         
@@ -71,7 +150,8 @@ def convert():
             chapter_as_string = str(chapter).zfill(ZFILL_LENGTH)
             page_as_string = re.findall(r'\d+', name)[-1].zfill(3)
             new_name = "{}-{}.jpg".format(chapter_as_string, page_as_string)
-            print(name + " -> " + new_name)
+            if DEBUG_BOOLEAN:
+                print(name + " -> " + new_name)
             
             # handle diplicates
             if os.path.isfile(new_name):
@@ -145,3 +225,9 @@ def to_cbz():
     for zip_file in glob_caught_folders:
         cbz_file = zip_file.replace(".zip", ".cbz")
         os.rename(zip_file, cbz_file)
+        
+def all():
+    convert()
+    move()
+    archive()
+    to_cbz()
